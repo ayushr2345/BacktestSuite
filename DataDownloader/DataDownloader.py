46cb8322af9c506f3a306c6549e3d4696fc876e8
ayushr2345 <ayush.r2345@gmail.com>
2025-05-04 16:18:59 +0530

    Fixed bugs, completed DataDownloader project. Added logging support, Added progress bars, fixed some bugs regarding what to do if data is already found

--- DataDownloader/DataDownloader.py
+++ DataDownloader/DataDownloader.py
@@ -2,12 +2,25 @@ import os
 import time
 import pyotp
 import sqlite3
+import logging
 import pandas as pd
 from py5paisa import FivePaisaClient
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-load_dotenv()
+from tqdm import tqdm
 
+# Load dotenv file
+load_dotenv()
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("DataDownloader.log", mode='w'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 class FivePaisaDownloader:
     def __init__(self, creds: dict, totp_secret: str):
@@ -45,7 +58,7 @@ class FivePaisaDownloader:
         return self.totp.now()
 
     def connect(self):
-        print("Connecting to 5Paisa...")
+        logger.info("Connecting to 5Paisa...")
         self.client = FivePaisaClient(cred = self.creds)
 
         for attempt in range(2):  # 2 attempts max: first try + one retry
@@ -57,13 +70,14 @@ class FivePaisaDownloader:
                 )
                 if resp is None:
                     raise Exception("Login response is None")
-                print("Logged in to 5Paisa successfully.")
+                logger.info("Logged in to 5Paisa successfully.")
                 break  # Successful login, exit loop
             except Exception as e:
                 if attempt == 0:
-                    print(f"Login failed, retrying in 30 seconds...\nError: {e}")
+                    logger.warning(f"Login failed, retrying in 30 seconds...\nError: {e}")
                     time.sleep(30)
                 else:
+                    logger.error("Could not log in to 5Paisa after retry. Please verify your credentials.")
                     raise Exception("Could not log in to 5Paisa after retry. Please verify your credentials.")
         self.check_and_update_scrip_master()
 
@@ -72,23 +86,24 @@ class FivePaisaDownloader:
         needs_update = True
 
         if not os.path.exists(self.data_path):
-            print(f"Directory {self.data_path} does not exist. Creating it")
+            logger.info(f"Directory {self.data_path} does not exist. Creating it")
             os.makedirs(self.data_path)
 
         if os.path.exists(file_path):
             modified_time = datetime.fromtimestamp(os.path.getmtime(file_path))
             if datetime.now() - modified_time < timedelta(days=7):
-                print("scrip_master.csv is up to date.")
+                logger.info("scrip_master.csv is up to date.")
                 needs_update = False
             else:
-                print("scrip_master.csv is older than 7 days. Updating...")
+                logger.info("scrip_master.csv is older than 7 days. Updating...")
 
         if needs_update:
             scrip_data_frame = self.client.get_scrips()
             if (scrip_data_frame is None):
+                logger.error("Could not fetch Scrip Master")
                 raise Exception(f"Could not fetch Scrip Master.")
             scrip_data_frame.to_csv(file_path, index=False)
-            print("scrip_master.csv has been updated.")
+            logger.info("scrip_master.csv has been updated.")
 
         self.setup_database()
         self.store_scrip_master_to_db(csv_path=file_path)
@@ -115,7 +130,7 @@ class FivePaisaDownloader:
         ''')
     
         conn.commit()
-        print(f"Database setup completed at {self.scrip_master}")
+        logger.info(f"Database setup completed at {self.scrip_master}")
         return conn
 
     def store_scrip_master_to_db(self, csv_path: str):
@@ -126,14 +141,14 @@ class FivePaisaDownloader:
         conn = sqlite3.connect(self.scrip_master)
         df.to_sql("scrip_master", conn, if_exists="replace", index=False)
         conn.close()
-        print("Scrip master stored to DB successfully.")
+        logger.info("Scrip master stored to DB successfully.")
 
     def get_scrip_code_by_name(self, name: str) -> int:
         conn = sqlite3.connect(self.scrip_master)
         cursor = conn.cursor()
 
-        print (f"Searching Scrip code for Scrip name: {name}")
-        print (f"Searching NSE for Scrip name: {name}")
+        logger.info (f"Searching Scrip code for Scrip name: {name}")
+        logger.debug (f"Searching NSE for Scrip name: {name}")
         # 1. Try exact match in NSE
         cursor.execute(
             "SELECT ScripCode FROM scrip_master WHERE Name = ? AND Exch = 'N' COLLATE NOCASE", 
@@ -141,7 +156,7 @@ class FivePaisaDownloader:
         )
         result = cursor.fetchone()
 
-        print (f"Searching BSE for Scrip name: {name}")
+        logger.debug (f"Searching BSE for Scrip name: {name}")
         # 2. If not found, try exact match in BSE
         if not result:
             cursor.execute(
@@ -151,7 +166,7 @@ class FivePaisaDownloader:
             result = cursor.fetchone()
 
         searched_partially = False
-        print (f"Searching NSE partially for Scrip name: {name}")
+        logger.debug (f"Searching NSE partially for Scrip name: {name}")
         # 3. If still not found, try partial match in NSE
         if not result:
             searched_partially = True
@@ -161,7 +176,7 @@ class FivePaisaDownloader:
             )
             result = cursor.fetchone()
 
-        print (f"Searching BSE partially for Scrip name: {name}")
+        logger.debug (f"Searching BSE partially for Scrip name: {name}")
         # 4. If still not found, try partial match in BSE
         if not result:
             searched_partially = True
@@ -172,7 +187,7 @@ class FivePaisaDownloader:
             result = cursor.fetchone()
 
         if searched_partially is True:
-            print (f"For Scrip name: {name}, the scrip code is partially searched with scrip code being: {result[0]}. Please verify that the correct Scrip is fetched!!! ")
+            logger.info (f"For Scrip name: {name}, the scrip code is partially searched with scrip code being: {result[0]}. Please verify that the correct Scrip is fetched!!! ")
         conn.close()
         return result[0] if result else None
 
@@ -192,19 +207,22 @@ class FivePaisaDownloader:
 
         if result:
             name, full_name, exch = result
-            print(f"ScripCode {scrip_code} corresponds to: {name} ({full_name}), Exchange: {exch}")
+            logger.info(f"ScripCode {scrip_code} corresponds to: {name} ({full_name}), Exchange: {exch}")
             return result
         else:
-            print(f"No scrip found with ScripCode {scrip_code}")
+            logger.error(f"No scrip found with ScripCode {scrip_code}")
             return None
 
 
     def validate_exchange_segment_and_time(self, time: str, Exch: str, ExchangeSegment: str):
         if time not in self.time_list:
+            logger.error("Invalid Time Frame, it should be within ['1m', '5m', '10m', '15m', '30m', '60m', '1d'].")
             raise Exception("Invalid Time Frame, it should be within ['1m', '5m', '10m', '15m', '30m', '60m', '1d'].")
         if Exch not in self.exch_map:
+            logger.error("Invalid Exchange, it should be within ['N', 'B', 'M', 'n'].")
             raise Exception("Invalid Exchange, it should be within ['N', 'B', 'M', 'n'].")
         if ExchangeSegment not in self.exchange_segment_map:
+            logger.error("Invalid Exchange Segment, it should be within ['c', 'd', 'u', 'x', 'y'].")
             raise Exception("Invalid Exchange Segment, it should be within ['c', 'd', 'u', 'x', 'y'].")
 
     def get_historical_data(self, Exch: str, ExchangeSegment: str, ScripNames: list, time: str, From: str, To: str):
@@ -218,45 +236,58 @@ class FivePaisaDownloader:
         """
 
         if (Exch == "" or ExchangeSegment == "" or len(ScripNames) == 0 or time == "" or From == "" or To == ""):
+            logger.error("Aruments missing. Please provide all the arguments")
             raise Exception("Aruments missing. Please provide all the arguments")
         if self.client is None:
+            logger.error("Aruments missing. Please provide all the arguments")
             raise Exception("Not connected. Call connect() first.")
         self.validate_exchange_segment_and_time(time, Exch, ExchangeSegment)
 
         fetched_scrip_codes = []
         for i in range(0, len(ScripNames)):
-            fetched_scrip_code = self.get_scrip_code_by_name(ScripNames[i])
+            fetched_scrip_code = self.get_scrip_code_by_name(ScripNames[i]) 
             if fetched_scrip_code is None:
-                raise Exception(f"No matches found for the scrip name ")
+                logger.error(f"No matches found for the scrip name {ScripNames[i]}")
+                raise Exception(f"No matches found for the scrip name {ScripNames[i]}")
             fetched_scrip_codes.append(fetched_scrip_code)
 
         if len(fetched_scrip_codes) != len(ScripNames):
+            logger.error("Could not find scrip codes for all the Scrip names provided")
             raise Exception("Could not find scrip codes for all the Scrip names provided")
 
-        scrip_codes_map = dict(zip(fetched_scrip_codes, ScripNames))
-        print("Printing Scrip Codes and their corresponding Names:")
+        scrip_names_corrected = []
+        for fetched_scrip_code in fetched_scrip_codes:
+            fetched_scrip_name = self.get_scrip_name_by_code(fetched_scrip_code)
+            if fetched_scrip_name is None:
+                logger.error(f"Could not find scrip name for scrip code: {fetched_scrip_code}")
+                raise Exception(f"Could not find scrip name for scrip code: {fetched_scrip_code}")
+            scrip_names_corrected.append(fetched_scrip_name[0])
+
+        scrip_codes_map = dict(zip(fetched_scrip_codes, scrip_names_corrected))
+        logger.info("Printing Scrip Codes and their corresponding Names:")
         for scrip_code in scrip_codes_map:
-            print (f"{scrip_code} : {scrip_codes_map[scrip_code]}")
+            logger.info (f"{scrip_code} : {scrip_codes_map[scrip_code]}")
 
         exchange_name = self.exch_map.get(Exch, Exch)
         exchange_segment_name = self.exchange_segment_map.get(ExchangeSegment, ExchangeSegment)
 
         if (time == '1d'):
-            for scrip_code in scrip_codes_map:
+            for scrip_code in tqdm(scrip_codes_map, desc="Downloading Daily Data"):
                 ScripCode = scrip_code
                 ScripName = scrip_codes_map[scrip_code]
-                print(f"Fetching data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
+                logger.debug(f"Fetching data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
                 file_name = f"data/{ScripName}/{ScripName}_{exchange_name}_{exchange_segment_name}_{time}_{From}_to_{To}.csv"
                 if (os.path.exists(file_name)):
-                    print(f"Data already exists for {ScripName} at {file_name}, skipping download.")
-                    return
+                    logger.info(f"Data already exists for {ScripName} at {file_name}, skipping download.")
+                    continue
 
                 data_frame = self.client.historical_data(Exch, ExchangeSegment, ScripCode, time, From, To)
                 if (data_frame is None):
+                    logger.error(f"Could not fetch historical data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
                     raise Exception(f"Could not fetch historical data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
                 if (data_frame.empty):
-                    print(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
-                    return
+                    logger.info(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
+                    continue
 
                 data_frame['Datetime'] = data_frame['Datetime'].apply(lambda x: datetime.strptime(str(x), "%Y-%m-%dT%H:%M:%S").date())
                 self.save_to_csv(data_frame, file_name)
@@ -274,22 +305,24 @@ class FivePaisaDownloader:
         """
         time_list = ['1m', '5m', '10m', '15m', '30m', '60m']
         if time not in time_list:
+            logger.error("Invalid Time Frame, it should be within ['1m', '5m', '10m', '15m', '30m', '60m'].")
             raise Exception("Invalid Time Frame, it should be within ['1m', '5m', '10m', '15m', '30m', '60m'].")
         if ScripCodesMap is None:
-            raise Exception(f"Empty Dict received in get_intraday_historocal_data()")
+            logger.error("Empty Dict received in get_intraday_historocal_data()")
+            raise Exception("Empty Dict received in get_intraday_historocal_data()")
 
         exchange_name = self.exch_map.get(Exch, Exch)
         exchange_segment_name = self.exchange_segment_map.get(ExchangeSegment, ExchangeSegment)
 
-        for scrip_code in ScripCodesMap:
+        for scrip_code in tqdm(ScripCodesMap, desc="Downloading Intraday Data"):
             ScripCode = scrip_code
             ScripName = ScripCodesMap[scrip_code]
             file_name = f"data/{ScripName}/{ScripName}_{exchange_name}_{exchange_segment_name}_{time}_{From}_to_{To}.csv"
             if (os.path.exists(file_name)):
-                print(f"Data already exists for {ScripName} at {file_name}, skipping download.")
-                return
+                logger.info(f"Data already exists for {ScripName} at {file_name}, skipping download.")
+                continue
 
-            print(f"Fetching intraday data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
+            logging.debug(f"Fetching intraday data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
 
             start_date = datetime.strptime(From, "%Y-%m-%d")
             end_date = datetime.strptime(To, "%Y-%m-%d")
@@ -301,9 +334,10 @@ class FivePaisaDownloader:
                 current_end_date = min(end_date, current_date + time_delta)
                 data_frame = self.client.historical_data(Exch, ExchangeSegment, ScripCode, time, current_date.strftime("%Y-%m-%d"), current_end_date.strftime("%Y-%m-%d"))
                 if (data_frame is None):
+                    logger.error(f"Could not fetch historical data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {current_date} to {current_end_date}.")
                     raise Exception(f"Could not fetch historical data for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {current_date} to {current_end_date}.")
                 if (data_frame.empty):
-                    print(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {current_date} to {current_end_date}.")
+                    logger.info(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {current_date} to {current_end_date}.")
                     current_date = current_end_date + timedelta(1)
                     continue
 
@@ -311,8 +345,9 @@ class FivePaisaDownloader:
                 current_date = current_end_date + timedelta(1)
 
             if (len(data_frames) == 0):
+                logger.error(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
                 raise Exception(f"No data found for {ScripName} belonging to Exchange {exchange_name} and segment {exchange_segment_name} for {time} interval from {From} to {To}.")
-            print(f"As time period is greater than 6 months: {From} to {To}, downloaded data in chunks of 6 months")
+            logger.info(f"As time period is greater than 6 months: {From} to {To}, downloaded data in chunks of 6 months")
             complete_data_frame = pd.concat(data_frames)
 
             self.save_to_csv(complete_data_frame, file_name)
@@ -320,9 +355,9 @@ class FivePaisaDownloader:
     def save_to_csv(self, df: pd.DataFrame, filename: str):
         directory = os.path.dirname(filename)
         if not os.path.exists(directory):
-            print(f"Directory {directory} does not exist. Creating it")
+            logger.debug(f"Directory {directory} does not exist. Creating it")
             os.makedirs(directory)
 
-        print(f"Saving to {filename}...")
+        logger.debug(f"Saving to {filename}...")
         df.to_csv(filename, index=False)
-        print("Saved successfully.")
+        logger.debug("Saved successfully.")
